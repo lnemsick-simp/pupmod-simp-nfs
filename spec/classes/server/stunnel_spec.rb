2749397dbc6b9ac6b36d524024224ceb6057a80c
lnemsick-simp <lnemsick.simp@gmail.com>
2020-02-21 16:18:20 -0500

    another unit test

--- spec/classes/server/stunnel_spec.rb
+++ spec/classes/server/stunnel_spec.rb
@@ -1,26 +1,42 @@
 require 'spec_helper'
 
-describe 'nfs::server::stunnel' do
-  on_supported_os.each do |os, facts|
-    context "on #{os}" do
-      let(:pre_condition) { 'class { "nfs": is_server => true }' }
-      let(:facts) { facts }
+# Testing private nfs::server::stunnel class via nfs class
+describe 'nfs' do
+  # What we are testing is not fact-dependent, but need facts for
+  # nfs class.  So, grab first set of supported OS facts.
+  let(:facts) { on_supported_os.to_a[0][1] }
+  let(:params) {{
+    # nfs class params
+    :is_server    => true,
+    :firewall     => true,
+    :stunnel      => true,
+    :tcpwrappers  => true,
+    :trusted_nets => [ '1.2.3.0/24' ]
+  }}
 
-      it { is_expected.to compile.with_all_deps }
-      it { is_expected.to contain_class('nfs::server') }
-      it { is_expected.to create_class('nfs::server::stunnel') }
-      if facts[:os][:release][:major] == '7'
-        it { is_expected.to create_stunnel__instance('nfs').with_systemd_wantedby([
-          'rpc-statd.service',
-          'nfs-mountd.service',
-          'nfs-rquotad.service',
-          'nfs-server.service',
-          'rpcbind.socket',
-          'nfs-idmapd.service',
-          'rpc-gssd.service',
-          'gssproxy.service',
-        ] ) }
-      end
-    end
-  end
+  it { is_expected.to compile.with_all_deps }
+  it { is_expected.to create_class('nfs::server::stunnel') }
+
+  it { is_expected.to create_stunnel__instance('nfsd').with( {
+    :client           => false,
+    :trusted_nets     => params[:trusted_nets],
+    :connect          => [2049],
+    :accept           => ['0.0.0.0:20490'],
+    :verify           => 2,
+    :socket_options   => ['l:TCP_NODELAY=1','r:TCP_NODELAY=1'],
+    :systemd_wantedby => [
+      'gssproxy.service',
+      'nfs-idmapd.service',
+      'nfs-mountd.service',
+      'nfs-server.service',
+      'rpc-gssd.service',
+      'rpc-rquotad.service',
+      'rpc-statd.service',
+      'rpc-statd-notify.service',
+      'rpcbind.service'
+     ],
+     :firewall         => true,
+     :tcpwrappers      => true,
+     :tag              => ['nfs']
+  } ) }
 end
