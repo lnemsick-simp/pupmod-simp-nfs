2cf2edb17a307ad4bca14298df3272d04f9fed58
lnemsick-simp <lnemsick.simp@gmail.com>
2020-02-25 15:37:15 -0500

    more tests

--- spec/classes/server/tcpwrappers_spec.rb
+++ spec/classes/server/tcpwrappers_spec.rb
@@ -11,7 +11,8 @@ describe 'nfs' do
           # nfs class params
           :is_server   => true,
           :nfsv3       => true,
-          :tcpwrappers => true
+          :tcpwrappers => true,
+          :trusted_nets => [ '1.2.3.0/24' ]
         }}
 
         it { is_expected.to compile.with_all_deps }
@@ -25,10 +26,21 @@ describe 'nfs' do
           it { is_expected.to_not create_tcpwrappers__allow('rquotad') }
         else
           it { is_expected.to create_class('tcpwrappers') }
-          it { is_expected.to create_tcpwrappers__allow('rpcbind') }
-          it { is_expected.to create_tcpwrappers__allow('statd') }
-          it { is_expected.to create_tcpwrappers__allow('mountd') }
-          it { is_expected.to create_tcpwrappers__allow('rquotad') }
+          it { is_expected.to create_tcpwrappers__allow('rpcbind').with_pattern(
+            params[:trusted_nets]
+          ) }
+
+          it { is_expected.to create_tcpwrappers__allow('statd').with_pattern(
+            params[:trusted_nets]
+          ) }
+
+          it { is_expected.to create_tcpwrappers__allow('mountd').with_pattern(
+            params[:trusted_nets]
+          ) }
+
+          it { is_expected.to create_tcpwrappers__allow('rquotad').with_pattern(
+            params[:trusted_nets]
+          ) }
         end
       end
 
@@ -37,8 +49,12 @@ describe 'nfs' do
           # nfs class params
           :is_server   => true,
           :nfsv3       => false,
-          :tcpwrappers => true
+          :tcpwrappers => true,
+          :trusted_nets => [ '1.2.3.0/24' ]
         }}
+
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to create_class('nfs::server::tcpwrappers') }
 
         if os_facts[:os][:release][:major].to_i > 7
           it { is_expected.to_not create_class('tcpwrappers') }
@@ -48,10 +64,15 @@ describe 'nfs' do
           it { is_expected.to_not create_tcpwrappers__allow('rquotad') }
         else
           it { is_expected.to create_class('tcpwrappers') }
-          it { is_expected.to create_tcpwrappers__allow('rpcbind') }
+          it { is_expected.to create_tcpwrappers__allow('rpcbind').with_pattern(
+            params[:trusted_nets]
+          ) }
+
           it { is_expected.to_not create_tcpwrappers__allow('statd') }
           it { is_expected.to_not create_tcpwrappers__allow('mountd') }
-          it { is_expected.to create_tcpwrappers__allow('rquotad') }
+          it { is_expected.to create_tcpwrappers__allow('rquotad').with_pattern(
+            params[:trusted_nets]
+          ) }
         end
       end
     end
